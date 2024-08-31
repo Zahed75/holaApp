abb4f4e95ed6f594fe52113d5d54af461c416eff
“Zahed75” <zahedhasan.cs@gmail.com>
2024-08-31 15:52:14 +0600

    Auth done

--- auths/urls.py
+++ auths/urls.py
@@ -11,10 +11,11 @@ from rest_framework_simplejwt.views import TokenVerifyView
 
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/register/',register_user),
     path('api/verify-otp/',verify_otp),
-    
+    path('api/resendOtp',resend_otp)
+
 
 ]
--- auths/views.py
+++ auths/views.py
@@ -92,3 +92,97 @@ def verify_otp(request):
             ]
         })
         
+@api_view(['POST'])
+def resend_otp(request):
+    phone_number = request.data.get('phone_number')
+    
+    try:
+        profile = UserProfile.objects.get(phone_number=phone_number)
+        
+        # Generate new OTP
+        otp = str(random.randint(1000, 9999))
+        
+        # Send OTP via SMS
+        message = f"Your new OTP code is {otp}. Please use this to verify your account."
+        response = send_sms(phone_number, message, API_KEY)
+        
+        # Update OTP and OTP creation time
+        profile.otp = otp
+        profile.otp_created_at = timezone.now()
+        profile.otp_verified = False
+        profile.save()
+        
+        return Response({
+            'phone_number': profile.phone_number,
+            'otp': otp,  # Optionally include the new OTP in the response
+            'message': 'New OTP sent to the phone number.'
+        }, status=status.HTTP_200_OK)
+    
+    except UserProfile.DoesNotExist:
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    except Exception as e:
+        return Response({
+            "code": status.HTTP_401_UNAUTHORIZED,
+            "message": str(e),
+            "status_code": 401,
+            "errors": [
+                {
+                    "status_code": 401,
+                    "message": str(e)
+                }
+            ]
+        })
+
+
+@api_view(['POST'])
+def tokenRefresh(request):
+    try:
+        payload = request.data
+        refresh = RefreshToken(token=payload.get('refresh_token'), verify=True)
+
+        return Response({
+            'access_token': str(refresh.access_token),
+            'refresh_token': str(refresh),
+            'token_type': str(refresh.payload['token_type']),
+            'expiry': refresh.payload['exp'],
+            'user_id': refresh.payload['user_id']
+        })
+    except Exception as e:
+        return Response({
+            "code": status.HTTP_401_UNAUTHORIZED,
+            "message": str(e),
+            "status_code": 401,
+            "errors": [
+                {
+                    "status_code": 401,
+                    "message": str(e)
+                }
+            ]
+        })
+
+
+@api_view(['POST'])
+def tokenVerify(request):
+    try:
+        payload = request.data
+        verify = UntypedToken(token=payload.get('access_token'))
+
+        return Response({
+            'access_token': str(verify.token),
+            'token_type': str(verify.payload['token_type']),
+            'expiry': verify.payload['exp'],
+            'user_id': verify.payload['user_id'],
+        })
+    except Exception as e:
+        return Response({
+            "code": status.HTTP_401_UNAUTHORIZED,
+            "message": str(e),
+            "status_code": 401,
+            "errors": [
+                {
+                    "status_code": 401,
+                    "message": str(e)
+                }
+            ]
+        })
