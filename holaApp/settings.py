eac5f39a5a5d19128e48e23fc67d491c8de4c8a0
“Zahed75” <zahedhasan.cs@gmail.com>
2024-09-08 14:08:31 +0600

    ProductModel Fixed

--- holaApp/settings.py
+++ holaApp/settings.py
@@ -90,7 +90,7 @@ if PRODUCTION:
             'NAME': 'holaGO',
             'USER': 'holaGO',
             'PASSWORD': 'DSpLFb4pWPEXFG7k',
-            'HOST': '127.0.0.1',
+            'HOST': 'localhost',
             'PORT': '3306',
         }
     }
